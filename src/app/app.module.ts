c08ea4250b9280656cae3007e74f021db6dfce53
rajsram <rajeshsivasankaran@outlook.com>
2019-04-13 13:55:11 +0530

    header designed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,16 +2,25 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
+import { MatMenuModule, MatIconModule } from '@angular/material';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {NoopAnimationsModule} from '@angular/platform-browser/animations';
+
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './shared/header/header.component';
 
 @NgModule({
-  imports: [BrowserModule, FormsModule,
+  imports: [BrowserModule,
+    FormsModule,
+    MatMenuModule,
+    MatIconModule,
+    BrowserAnimationsModule,
+    NoopAnimationsModule
     // RouterModule.forRoot(new Routes = [
     //   {
 
     //   }], { useHash: true })
-    ],
+  ],
   declarations: [AppComponent, HeaderComponent],
   bootstrap: [AppComponent]
 })
