fe7c6092bbaf8536225062285b4584267d8ac4b0
rajsram <rajeshsivasankaran@outlook.com>
2019-04-23 09:12:45 +0530

    user model

--- src/app/user/models/user.ts
+++ src/app/user/models/user.ts
@@ -1,2 +1,36 @@
 export class User {
+  Name: string;
+  UserId: string;
+  Password: string;
+  EmailId: string;
+  Mobile: string;
+  Address: string;
+  Addresstype: AddressType;
+  Usertype: UserType;
+  Roletype: RoleType;
+  Status: UserStatus
+  Trait: UserTrait;
+}
+
+export enum AddressType {
+  Home,
+  Work,
+  Others
+}
+
+export enum RoleType {
+  Admin,
+  Registered,
+  Guest
+}
+
+export enum UserStatus {
+  Enabled,
+  Disabled
+}
+
+export enum UserType {
+}
+
+export enum UserTrait {
 }
